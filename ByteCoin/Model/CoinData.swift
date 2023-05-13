470641d32ce6d56cff1dd68687e2bb2cede077a4
Snooopyman <armando.snoop@gmail.com>
2023-05-13 23:28:00 +0200

    App finished adding protocol, delegate and using extension in the ViewController

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Armando Cáceres on 13/5/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
